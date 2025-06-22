91e52f10d53d7d5b2f022d73f02a59ae380f9605
Evancelj <w.lexandra@gmail.com>
2025-06-22 14:43:37 +0300

    issue-26: changes according VicBocanegra25 comments

--- src/main/java/com/hyperskill/repository/PlayerRepository.java
+++ src/main/java/com/hyperskill/repository/PlayerRepository.java
@@ -1,16 +1,16 @@
 package com.hyperskill.repository;
 
-import com.hyperskill.entity.Person;
 import com.hyperskill.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
-    Player getById(Long id);
-    Player getByFirstNameAndLastName(Person person);
-    Collection<Player> getAll();
+    Optional<Player> findById(Long id);
+    Player findByFirstNameAndLastName(String firstName, String lastName);
+    List<Player> findAll();
     void deleteById(Long id);
 }
