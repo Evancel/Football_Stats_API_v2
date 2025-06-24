8efbe5f7bf31dcd3f127c3010fa488ca24581fbd
Pramod Bharti <pramodbharti.info@gmail.com>
2025-06-24 15:50:24 +0530

    Added repo and service methods to perform crud ops on team, also handled exceptions

--- src/main/java/com/hyperskill/repository/TeamRepository.java
+++ src/main/java/com/hyperskill/repository/TeamRepository.java
@@ -2,9 +2,10 @@ package com.hyperskill.repository;
 
 import com.hyperskill.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+@Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
-    List<Team> findAll();
+    boolean existsByName(String name);
+    boolean existsByNameAndIdNot(String name, Long id);
 }
