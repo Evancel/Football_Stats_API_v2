51ba80ce1f69e8df08dd734c586a2fc393026898
Evancelj <w.lexandra@gmail.com>
2025-07-02 18:42:17 +0300

    exclude unnecessary imports

--- src/main/java/com/hyperskill/repository/PlayerRepository.java
+++ src/main/java/com/hyperskill/repository/PlayerRepository.java
@@ -1,9 +1,6 @@
 package com.hyperskill.repository;
 
-import com.hyperskill.model.dto.PlayerResponseDTO;
 import com.hyperskill.model.entity.Player;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
